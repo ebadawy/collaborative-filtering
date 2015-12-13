7ba671c2b8e30ae1de990b7cd3a918190cc00c5f
ebadawy <ehalbadawy93@gmail.com>
2015-12-13 13:51:44 +0200

    Implement item bias function

---
+++ itemBias.m
@@ -0,0 +1,11 @@
+function bi = itemBias(ua, Iua, mu, dataSet)
+
+bi = zeros(max(Iua),1);
+
+for i=1:size(Iua)(1)
+  item = Iua(i,2);
+  Ui = dataSet(dataSet(:,2)==item,:);
+  bi(item) = mean(Ui(:,3)) - mu;
+end
+
+end
